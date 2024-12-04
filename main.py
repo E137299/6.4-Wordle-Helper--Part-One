28243ed8ff190adb563a5ca05089678f6f32dd7c
David Yin <29613693+E137299@users.noreply.github.com>
2024-12-04 14:09:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+def print_wordbank(wordbank):
+    for i in range(0, len(wordbank),10):
+        print('   '.join(wordbank[i:i+10]))
+
 '''
 Import New York Times' wordbank
 '''
 words = open("wordbank.txt","r") #opens text file for reading
 wordbank = words.read() #stores contents of txt file in variable
 wordbank = wordbank.split() #place words into a list.
+
+print_wordbank(wordbank)
